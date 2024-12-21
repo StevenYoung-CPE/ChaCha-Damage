8bfc1cb6f3762bb2c28b8ae04656d99ab1c046b1
StevenYoung-CPE <157507330+StevenYoung-CPE@users.noreply.github.com>
2024-12-21 13:53:36 -0600

    Stuff and things
    Fixed damage implementation

--- Damage Calculator.py
+++ Damage Calculator.py
@@ -1,32 +1,54 @@
 import random
 from Pokemons import Pokemon
 # Pokemon are defined by Pokemon(Lvl,Atk,Def,Spatk,Spdef), For sure for sure
-with open('Pokemon List.txt','r') as PL:
-    print(PL.read())
-    for line in PL:
-        mon = PL.readline()
-        mon = mon.split(' ')
-        print(mon)
 
+with open('Pokemon List.txt', 'r') as PL:
+    Plist = []
+    for line in PL:
+        mon = line.strip().split()
+        name, lvl, atk, defense, spatk, spdef = mon
+        Plist.append(Pokemon(name, int(lvl), int(atk), int(defense), int(spatk), int(spdef)))   
+def pfinder(Pokedex,name):
+    for poke in Pokedex:
+        if poke.Name == name:
+            return poke
+    return None
 def damage(Lvl,atk,Def,dice,sb,eff,crit,misc):
     #Currently built to autoroll dice, just input the number of dice that should be rolled
     #Sb stands for stab, Use 1 for non stab, 1.2 for stab, and 2 for adaptability
     #Crit is a binary, 'y' for crit occured.
+    dice = int(dice/5)
     x = 0
     if crit == 'y':
         for i in range(2*dice):
             x = x + random.randint(1,8)
-            print(x)
     else:
         for i in range(dice):
             x = x + random.randint(1,8)
-            print(x)
+    print(x)
     return ((10*Lvl+10)/250*atk/Def*x)*sb*eff*misc
-#print(damage(1,1,1,5,1.5,1,1,'n'))
 def main():
-    battle = 0
-    while battle == 0:
-        Atker = input('Who is attacking?')
-        PS = input('Physical Or Special (P/S)') #Respond with p or s
-        if PS == 'p':
-            pass
+        '''Userinput = input('Attacker, Attack stages, P/S, Defender, Defense Stages, BP, Stab, Type effectiveness, Misc Mult, Crit: ')
+        Inlist = Userinput.split(',')
+        Atker, AtkUp, PS, Defer, Defup, BP, SB, Eff, misc, Cr = Inlist'''
+        Atker = input("Who attack? ")
+        AtkUp = input('(-6 to 6)')
+        PS = input('Physical or Special (p,s)').lower()
+        Defer = input('Who Def? ')
+        Defup = input("-6 to 6")
+        BP = input('Move Basepower? ')
+        SB = input("stab multiplier (1, 1.5, etc)")
+        Eff = input("type multiplier?")
+        misc = input('Additional multipliers')
+        Cr = input ('crit? (y,n)').lower()
+        Atker = pfinder(Plist,Atker)
+        Defer = pfinder(Plist,Defer)
+        if PS.lower() == 'p':
+            Atker.StatChange(int(AtkUp),'Atk')
+            Defer.StatChange(int(Defup),'Def')
+            print(damage(Atker.Level,Atker.Atk,Defer.defense,int(BP),float(SB),int(Eff),Cr,float(misc)))
+        if PS.lower() == 's':
+            Atker.StatChange(int(AtkUp),'Spatk')
+            Defer.StatChange(int(Defup),'Spdef')
+            print(damage(Atker.Level,Atker.Spatk,Defer.Spdef,BP,SB,Eff,Cr,misc))
+main()
--- Pokemons.py
+++ Pokemons.py
@@ -1,10 +1,11 @@
 class Pokemon:
-    def __init__(self,Lvl,Atk,Def,Spatk,Spdef):
+    def __init__(self,Name,Lvl,Atk,Def,Spatk,Spdef):
         self.Level = Lvl
         self.Atk = Atk
         self.defense = Def
         self.Spatk = Spatk
         self.Spdef = Spdef
+        self.Name = Name
     def StatChange(self,num,stat):
         StageList = [2/8,2/7,2/6,2/5,2/4,2/3,2/2,3/2,4/2,5/2,6/2,7/2,8/2]
         mult = StageList[6+num]
@@ -16,4 +17,6 @@ class Pokemon:
             self.Spatk = self.Spatk*mult
         if stat == 'Spdef':
             self.Spdef = self.Spdef*mult
-Bisharp = Pokemon(9,128,113,69,76)
+        return self
+    def __repr__(self):
+        return f"{self.Name} (Lvl: {self.Level}, Atk: {self.Atk}, Def: {self.defense}, SpAtk: {self.Spatk}, SpDef: {self.Spdef})"
